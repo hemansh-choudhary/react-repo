27daa9c83ea49cd871597afcd5c3776ec6002a51
hemansh choudhary <hemansh.choudhary@in.unisys.com>
2020-09-24 14:19:53 +0530

    remaining file

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React, { Component } from 'react';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
+import Checkout from './containers/Checkout/Checkout';
+
+import { Route,Switch } from 'react-router-dom';
+
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <BurgerBuilder />
+          <Switch>
+            <Route path='/' exact component={BurgerBuilder} />
+            <Route path='/checkout' component={Checkout} />
+          </Switch>
+          
+          {/* <BurgerBuilder />
+          <Checkout /> */}
         </Layout>
       </div>
     );
